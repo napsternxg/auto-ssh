2d6b6fa1ce6895dfd10fbbb6e02b6082dcc2c35d
napsternxg <shubhanshumishra@gmail.com>
2013-02-06 11:56:26 +0530

    Use relative paths for config and enterPass.sh files while calling scripts.sh from auto-ssh. Also check if config file is empty or not. Give error in case of empty config file.

--- scripts.sh
+++ scripts.sh
@@ -1,18 +1,26 @@
 # Script to connec to ssh clients.
-
+DIRNAME=$(dirname $0)
+AUTO_SSH_CONFIG=$DIRNAME"/config"
+EXPECT_SCRIPT=$DIRNAME"/enterPass.sh"
+echo "Path of config file: "$AUTO_SSH_CONFIG
 # ./config is the file which contains all the connection setting values for all machines.
 menu(){
 	echo "Following machines are available for connection:";
 	
 	# Create an array settings using each line from config file.
-	mapfile -t settings < "./config"
+	mapfile -t settings < $AUTO_SSH_CONFIG
+	
+	if [ $#{settings[@]} == 0 ]; then
+		echo "Error reading config file."
+		exit 1
+	fi
 	
 	# Store the total number of configurations given in the file.
 	connections=${#settings[@]};
 	echo ${settings[2]};
 	
 	# Display all the setting values from the config file.
-	grep -ine ".*" ./config | cut -d " " -f 1;
+	grep -ine ".*" $AUTO_SSH_CONFIG | cut -d " " -f 1;
 }
 
 input(){
@@ -24,12 +32,12 @@ input(){
 	echo "Connecting to $ip using USER: $user ...";
 
 	# Call the expect script to do the auto login using the username, password and ip from the file for the given machine.
-	./enterPass.sh $ip $user $passwd
+	$EXPECT_SCRIPT $ip $user $passwd
 }
 
 #Print Menu
 menu;
-
+echo "Arguments: $# and $@"
 #Prompt user for input.
 read -p "Enter your choice[number]: " connect
 #Connect to the specified machine.
