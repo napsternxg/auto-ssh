21a831745dd0426e55d5543888c15a66507e6187
napsternxg <shubhanshumishra@gmail.com>
2013-02-07 15:21:24 +0530

    Add support for adding RSA fingerprint to unknown host and then recursively send the password. Fix bug which disallow login if host is unknown and user is required to enter password manually.

--- enterPass.sh
+++ enterPass.sh
@@ -7,12 +7,18 @@ set password [lindex $argv 2]
 #Create the ssh session using the username, password and ip of the machine selected.
 
 spawn /usr/bin/ssh -l $user $ip
+
 expect {
+	-re ".*Are you sure you want to continue connecting.*" {
+		send "yes\n"
+		exp_continue
+	}
+
+	
 	-re ".*assword:" {
 		send $password
 		send "\n"
 		interact
-	}
+	} 
 }
-
 #Exit expect script.
