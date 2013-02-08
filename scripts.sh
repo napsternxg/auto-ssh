dd8fa999f994fa4027a6681c764994e96cdd9c75
napsternxg <shubhanshumishra@gmail.com>
2013-02-08 17:32:28 +0530

    Added config menu to the script

--- scripts.sh
+++ scripts.sh
@@ -35,11 +35,49 @@ input(){
 	$EXPECT_SCRIPT $ip $user $passwd
 }
 
-#Print Menu
-menu;
-echo "Arguments: $# and $@"
-#Prompt user for input.
-read -p "Enter your choice[number]: " connect
-#Connect to the specified machine.
-input $connect
+config(){
+	
+	read -p "Connection Name: " connection
+	read -p "IP: " ip
+	read -p "Username: " user
+	read -s -p "Password: " passwd
+	if [-z $passwd ]; then
+		passwd="null"
+	fi
+	echo "$connection ssh $user $passwd $ip" >> $AUTO_SSH_CONFIG
+	echo "Configured $connection in $AUTO_SSH_CONFIG."
+	return
+}
+
+init_app(){
+	n_args=$#
+	s_args=$@
+	echo "Total arguments: $n_args"
+	echo "Value of args: $s_args"
+	if [ $n_args -lt 1 ]; then
+		#Print Menu
+		menu;
+		echo "Arguments: $# and $@"
+		#Prompt user for input.
+		read -p "Enter your choice[number]: " connect
+		#Connect to the specified machine.
+		input $connect
+	else
+		case ${s_args[0]} in
+		"config")
+			echo "Configure a new connection:\n"
+			config
+			;;
+		"remove")
+			echo "Removing setting"
+			;;
+		esac
+		echo "Finished config block"
+	fi
+}
+
+init_app $@
+
+
+
 
